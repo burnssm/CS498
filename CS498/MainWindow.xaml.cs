c6c26e21e23b44273bd25bc99f538d756b696079
burnssm <burnssm@msoe.edu>
2015-01-07 20:14:17 -0600

    Pushed initial UI

---
+++ CS498/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace CS498
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBox_Loaded(object sender, RoutedEventArgs args)
+        {
+            var dataList = new List<string>();
+            for (var i = 1; i <= 12; i++)
+            {
+                dataList.Add(i.ToString());
+            }
+
+            var box = sender as System.Windows.Controls.ComboBox;
+            if (box != null) box.ItemsSource = dataList;
+        }
+    }
+}
