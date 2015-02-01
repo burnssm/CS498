6d069934aa79fd6400cdab02bf771bc6785d1968
burnssm <burnssm@msoe.edu>
2015-01-31 19:43:20 -0600

    Created the Save and Cancel functionality

--- CS498/MainWindow.xaml.cs
+++ CS498/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using CS498.Lib;
 using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Calendar = CS498.Lib.Calendar;
 
 
 namespace CS498
@@ -10,36 +14,89 @@ namespace CS498
     /// </summary>
     public partial class MainWindow
     {
+        private ObservableCollection<GoogleEvent> _events;
+        private ObservableCollection<OpenTimeBlocks> _timeBlocks;
         public MainWindow()
         {
             Calendar.Authorize();
             InitializeComponent();
+            _events = new ObservableCollection<GoogleEvent>();
+            _timeBlocks = new ObservableCollection<OpenTimeBlocks>();
             AddDummyTasks();
         }
 
         private void AddDummyTasks()
         {
-            var tasks = new List<GoogleEvent>();
-            var timeblocks = new List<OpenTimeBlocks>();
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 10; i++)
             {
-                tasks.Add(new GoogleEvent
+                _events.Add(new GoogleEvent
                 {
                     Title = "Hello" + i,
                     StartDateTime = DateTime.Now,
                     EndDateTime = DateTime.Now.AddHours(2),
                     Description = "Test"
                 });
-                timeblocks.Add(new OpenTimeBlocks
+                _timeBlocks.Add(new OpenTimeBlocks
                 {
                     StartDateTime = DateTime.Now,
                     EndDateTime = DateTime.Now.AddHours(2)
                 });
             }
 
-            TaskList.ItemsSource = tasks;
-            GoogleList.ItemsSource = timeblocks;
+            TaskList.ItemsSource = _events;
+            GoogleList.ItemsSource = _timeBlocks;
         }
 
+        private void TaskDates_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO Check if Calendar is in list or get it
+        }
+
+        private void GoogleDate_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO Get TimeBlocks for the selected time
+        }
+
+        private void Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            var taskName = TaskName.Text;
+            var description = Description.Text;
+
+            var selectedTimeBlock = (OpenTimeBlocks)GoogleList.SelectedItem;
+
+            var hour = Hours.Value ?? 0;
+            var minutes = Minutes.Value ?? 0;
+
+            if (selectedTimeBlock != null)
+            {
+                var newEvent = new GoogleEvent
+                {
+                    Title = taskName,
+                    StartDateTime = selectedTimeBlock.StartDateTime,
+                    EndDateTime = selectedTimeBlock.StartDateTime.AddHours(hour).AddMinutes(minutes),
+                    Description = description
+                };
+
+                _events.Add(newEvent);
+                //TODO Create the event on the calendar
+            }
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+
+            TaskName.Clear();
+            Description.Text = "";
+            DateTimePicker.Text = "";
+            GoogleList.UnselectAll();
+            Hours.Value = 0;
+            Minutes.Value = 0;
+        }
+
+        private void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearForm();
+        }
     }
 }
